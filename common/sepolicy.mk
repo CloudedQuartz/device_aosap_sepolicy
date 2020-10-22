84aaddbb28af26cb6dfbb854214aaa78a10b068f
Aayush Gupta <aayushgupta219@gmail.com>
2020-10-22 13:02:38 +0530

    sepolicy: Switch to BOARD_VENDOR_SEPOLICY_DIRS
    - BOARD_SEPOLICY_DIRS is deprecated and gives compile-time
      errors when used in unison with a device using BOARD_VENDOR_SEPOLICY_DIRS
    
    Ref:
    [0]: https://github.com/LineageOS/android_system_sepolicy/commit/ec3ac470a98342c13c1fec8d46433c73b08531be
    
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>
    Change-Id: Icefb062cc8cdef532b4310684d9a66afe97e49c4
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>
    Signed-off-by: CloudedQuartz <ravenklawasd@gmail.com>

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -25,7 +25,7 @@ ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     device/aosap/sepolicy/common/dynamic
 else
-BOARD_SEPOLICY_DIRS += \
+BOARD_VENDOR_SEPOLICY_DIRS += \
     device/aosap/sepolicy/common/dynamic \
     device/aosap/sepolicy/common/vendor
 endif
--- exynos/sepolicy.mk
+++ exynos/sepolicy.mk
@@ -17,7 +17,7 @@ BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     device/aosap/sepolicy/exynos/dynamic \
     device/aosap/sepolicy/exynos/system
 else
-BOARD_SEPOLICY_DIRS += \
+BOARD_VENDOR_SEPOLICY_DIRS += \
     device/aosap/sepolicy/exynos/dynamic \
     device/aosap/sepolicy/exynos/vendor
 endif
